129e6e3857f53a2d168bc1acf9ea460381e01b9b
Willem Wyndham <willem@cs.umd.edu>
2020-02-21 11:25:52 -0500

    cleaned up more
    Removed gulp and used preferred compiler file `asconfig.js`

---
+++ asconfig.js
@@ -0,0 +1,12 @@
+const compile = require("near-bindgen-as/compiler").compile
+
+compile("assembly/index.ts", // input file
+        "dist/main.wasm",    // output file
+        [
+        //   "-O1",          // Optional arguments
+        // "--debug",
+        "--measure",         // Shows compiler runtime
+        "--validate"         // Validate the generated wasm module
+        ], {
+          verbose: true     // Output the cli args passed to asc
+        });
