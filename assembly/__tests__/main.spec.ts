8a3de4cc44fea6896c28ff96c1adf6e2afa33bfe
Willem Wyndham <willem@cs.umd.edu>
2020-02-21 10:49:08 -0500

    Updated test example and added .gitpod file

--- assembly/__tests__/main.spec.ts
+++ assembly/__tests__/main.spec.ts
@@ -1,18 +1,52 @@
 import {
     getCounter,
-    resetCounter
+    resetCounter,
+    incrementCounter,
+    decrementCounter
 } from '../main';
 
-describe("smart contract",() => {
-    // getCounter(): i32 {
-    it("should respond to getCounter", () => {
-        expect(getCounter()).toStrictEqual(0)
-        // expect(getCounter()).toBeNull(); // uncomment this line to see the SPECIFIC compilation error
+import { VM } from "wasm-mock-vm";
+
+describe("Counter ", () => {
+    it("should increment by one", () => {
+        incrementCounter(1);
+        expect(getCounter()).toBe(1);
+    });
+
+    it("should decrement by one", () => {
+        decrementCounter(1);
+        expect(getCounter()).toBe(0);
+    });
+
+    it("should be resetable", () => {
+        incrementCounter(1);
+        incrementCounter(1);
+        resetCounter(); // reset to zero
+        expect(getCounter()).toBe(0); 
+    });
+
+    describe("With reset state", () => {
+        // Save the state before all tests
+        beforeAll(() => {
+            VM.saveState();
+        });
+
+        // Restore the state after each test
+        afterEach(() => {
+            VM.restoreState();
+        });
+
+        it("should increment by one and restore VM state after", () => {
+            incrementCounter(1);
+            expect(getCounter()).toBe(1);
+        });
+    
+        it("should decrement by one and start at zero", () => {
+            decrementCounter(1)
+            expect(getCounter()).not.toBe(0);
+            expect(getCounter()).toBe(-1);
+        });
     })
-    // resetCounter(): void {
-    it("should respond to resetCounter", () => {
-        // expect(resetCounter()).toBeNull(); // uncomment this line to see the VAGUE compilation error
-    })
-})
+});
 
 
