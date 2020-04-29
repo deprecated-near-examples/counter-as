b9df381d97d362272458489c82fe253ab5f5b8cf
Willem Wyndham <willem@cs.umd.edu>
2020-04-29 11:53:44 -0400

    Update near-sdk and update tests

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,11 +1,43 @@
-const Runtime = require("near-sdk-as/vm").Runtime;
+const Runtime = require("near-sdk-as/vm/dist").Runtime;
 const path = require("path");
-const runtime = new Runtime();
 
 
-test("Counter", () => {
-  const alice = runtime.newAccount("alice");
-  const counter = runtime.newAccount("counter", path.join(__dirname,"../out/main.wasm"));
-  expect(alice.call_other("counter", "incrementCounter", {value: 1}).result.outcome.logs[0]).toBe("Counter is now: 1");
-  expect(counter.call("getCounter").return_data).toBe(1, "counter should increment");
-})
+
+// test("Counter", () => {
+//   const alice = runtime.newAccount("alice");
+//   const counter = runtime.newAccount("counter", );
+//   expect(alice.call_other("counter", "incrementCounter", {value: 1}).result.outcome.logs[0]).toBe("Counter is now: 1");
+//   expect(counter.call("getCounter").return_data).toBe(1, "counter should increment");
+// });
+
+const contractPath = path.join(__dirname,"../out/main.wasm");
+
+describe('Token', function () {
+  let runtime;
+  let contract;
+
+  function getCounter() {
+    return parseInt(contract.view("getCounter").return_data);
+  }
+
+  beforeAll(function () {
+    runtime = new Runtime();
+    contract = runtime.newAccount("counter", contractPath);
+  });
+
+  describe('counter', function () {
+    it('can be incremented', function () {
+      const startCounter = getCounter() 
+      contract.call("incrementCounter",{value: 1});
+      const endCounter = getCounter()
+      expect(endCounter).toEqual(startCounter + 1);
+    });
+    it('can be decremented', async function () {
+      contract.call("incrementCounter",{value: 1});
+      const startCounter = getCounter()
+      contract.call("decrementCounter",{value: 1});
+      const endCounter = getCounter()
+      expect(endCounter).toEqual(startCounter - 1);
+    });
+  });
+});
